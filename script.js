f00c29dade7bff8dc801ca3b9dbb750d3b251987
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-29 14:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let arr = [2,1,2];
+let n = arr.length;
+let max = Math.floor(n/2);
+let ans = 0;
+let obj = {};
+for ( let i = 0; i < arr.length; i++ ){
+	if(obj[arr[i]] != "undefined"){
+		obj[arr[i]] = obj[arr[i]] + 1;
+	}
+	else{
+		obj[arr[i]] = 1;
+	}
+}
+
+for ( let i in obj ){
+	if(obj[i] > max){
+		ans = obj[i];
+	}
+}
+return ans;
